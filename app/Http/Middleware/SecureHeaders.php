70f61b57903b32b9bd23184380a5908140cc4db0
MichaelRushton <mikerushton@hotmail.co.uk>
2026-09-11 12:49:09 +0100

    Add secure headers middleware

---
+++ app/Http/Middleware/SecureHeaders.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SecureHeaders
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        $this->setContentSecurityPolicyHeader($response);
+        $this->setPermissionsPolicyHeader($response);
+        $this->setReferrerPolicyHeader($response);
+        $this->setStrictTransportSecurityHeader($response);
+        $this->setXContentTypeOptionsHeader($response);
+        $this->setXFrameOptionsHeader($response);
+        $this->setXXSSProtectionHeader($response);
+
+        return $response;
+    }
+
+    protected function setContentSecurityPolicyHeader(Response $response): void
+    {
+        // $response->headers->set('Content-Security-Policy', "default-src 'self'");
+    }
+
+    protected function setPermissionsPolicyHeader(Response $response): void
+    {
+        $response->headers->set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
+    }
+
+    protected function setReferrerPolicyHeader(Response $response): void
+    {
+        $response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
+    }
+
+    protected function setStrictTransportSecurityHeader(Response $response): void
+    {
+        $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+    }
+
+    protected function setXContentTypeOptionsHeader(Response $response): void
+    {
+        $response->headers->set('X-Content-Type-Options', 'nosniff');
+    }
+
+    protected function setXFrameOptionsHeader(Response $response): void
+    {
+        $response->headers->set('X-Frame-Options', 'DENY');
+    }
+
+    protected function setXXSSProtectionHeader(Response $response): void
+    {
+        $response->headers->set('X-XSS-Protection', '1; mode=block');
+    }
+}
